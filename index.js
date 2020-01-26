57e9e6dfd646fe57fd866f86ec45b97bd2c8e61f
hilarious-tier-9201 <dhirahapani@hotmail.com>
2020-01-26 02:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-  } else (string.toUpperCase() === string) {
-    return "YES, INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+    return 'I can\t hear you!';
+  }
+  if (string.toUpperCase() === string) {
+    return 'YES, INDEED!';
+  }
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
   }
 }
