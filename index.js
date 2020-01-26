37500a0f24fa912f464a7fe967ecf3d475110c5f
hilarious-tier-9201 <dhirahapani@hotmail.com>
2020-01-26 02:20:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(Hello) {
+  return Hello.toLowerCase()
+}
+function logshout(string) {
+  console.log(string.toUpperCase());
+}
